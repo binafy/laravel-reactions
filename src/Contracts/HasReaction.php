ff3176ee47444ba9797d161b05a95ca1bd024cb5
Milwad <milwad.dev@gmail.com>
2024-09-21 15:24:29 +0330

    Update HasReaction.php

--- src/Contracts/HasReaction.php
+++ src/Contracts/HasReaction.php
@@ -8,4 +8,9 @@ interface HasReaction
      * Relation morph.
      */
     public function reactions(): \Illuminate\Database\Eloquent\Relations\MorphMany;
+
+    /**
+     * Get the value of the model's primary key.
+     */
+    public function getKey();
 }
