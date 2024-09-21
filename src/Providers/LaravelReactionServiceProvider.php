795a50a6ed6c0203845b4239050ff0ff200bbfd4
Milwad <milwad.dev@gmail.com>
2024-09-21 15:00:52 +0330

    Create LaravelReactionServiceProvider.php

---
+++ src/Providers/LaravelReactionServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Binafy\LaravelReaction\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelReactionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../../config/reaction.php', 'laravel-reactions');
+    }
+}
