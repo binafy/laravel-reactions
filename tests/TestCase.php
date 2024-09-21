76be252a94e4c23f744b77df105815d3a5780d4e
Milwad <milwad.dev@gmail.com>
2024-09-21 15:38:15 +0330

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,51 @@
 
 namespace Tests;
 
-use PHPUnit\Framework\TestCase as BaseTestCase;
+use Binafy\LaravelReaction\Providers\LaravelReactionServiceProvider;
+use Illuminate\Encryption\Encrypter;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\SetUp\User;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
-    //
+    use RefreshDatabase;
+
+    /**
+     * Load package service provider.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [LaravelReactionServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Set default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        // Set app key
+        $app['config']->set('app.key', 'base64:'.base64_encode(
+            Encrypter::generateKey(config()['app.cipher'])
+        ));
+
+        // Set user model
+        $app['config']->set('auth.providers.users.model', User::class);
+
+        // Set user model for monitoring config
+        $app['config']->set('laravel-reactions.user.model', User::class);
+    }
 }
