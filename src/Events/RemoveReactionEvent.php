d6e9bc4c6da5c82c7118f512e06bdc5ea4e90144
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-07-17 14:13:25 +0330

    Update RemoveReactionEvent.php

--- src/Events/RemoveReactionEvent.php
+++ src/Events/RemoveReactionEvent.php
@@ -21,8 +21,6 @@ class RemoveReactionEvent
 
     /**
      * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
      */
     public function broadcastOn(): array
     {
